2dffa809ac4aa21ef4bb2dcca610e652cc3bba55
v0agent <it+v0agent@vercel.com>
2026-07-01 18:07:29 +0000

    feat: add Facebook Pixel tracking script to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ variable: '--font-geist-sans', subsets: ['latin'] })
@@ -44,6 +45,27 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${inter.variable} bg-[#f0ebe4]`}>
       <body className="font-sans antialiased bg-[#f0ebe4]">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1806336004082457');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1806336004082457&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
